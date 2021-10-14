4608865468253bff18526ae5a61717c353b019d5
silasmakou <silasmakou@gmail.com>
2021-10-13 21:46:34 -0300

    atualizando repositorio

---
+++ 01-EstruturaDoPrograma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EstruturaDoPrograma.Exemplos;
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var s = new Pilha();
+            s.Empilha(2);
+            s.Empilha(20);
+            s.Empilha(200);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
